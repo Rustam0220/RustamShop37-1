f7409df8145af0b4e7e557da57b90859a55efab7
Rustam0220 <rustam20021015@gmail.com>
2024-02-23 00:15:36 +0600

    hw4

--- PycharmProjects/Rustam_37-1/shop/settings.py
+++ PycharmProjects/Rustam_37-1/shop/settings.py
@@ -138,6 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
     "site_title": "Rustam_Admin",
@@ -279,6 +280,7 @@ JAZZMIN_SETTINGS = {
     # Add a language dropdown into the admin
     "language_chooser": False,
 }
+
 
 
 
